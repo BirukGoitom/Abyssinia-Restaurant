d0de2aeba894a314b2f1c4a5b588117f6f7e1afb
copernicus2070-pixel <copernicus2070@gmail.com>
2026-06-24 17:43:49 +0300

    Added RestaurantOwner.java class

---
+++ model/RestaurantOwner.java
@@ -0,0 +1,15 @@
+public class RestaurantOwner extends User {
+
+    public RestaurantOwner(int id, String name, String username, String password) {
+        super(id, name, username, password);
+    }
+
+    public RestaurantOwner(String name, String username, String password) {
+        super(name, username, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "RestaurantOwner";
+    }
+}
