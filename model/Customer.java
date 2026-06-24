78d38e3f27b9d71c79f22fa663d4a86327112ba8
copernicus2070-pixel <copernicus2070@gmail.com>
2026-06-24 17:46:10 +0300

    Added Customer.java class

---
+++ model/Customer.java
@@ -0,0 +1,14 @@
+public class Customer extends User {
+
+    public Customer(String name, String username) {
+        super(name, username, "");
+    }
+
+    public Customer(String name, String username, String password) {
+        super(name, username, password);
+    }
+
+    public String getRole() {
+        return "Customer";
+    }
+}
